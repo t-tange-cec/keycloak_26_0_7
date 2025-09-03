619f5e400fa2f9154f28ca0dc943a31059f9a9ff
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-09-03 13:12:43 +0900

    ソース

---
+++ keycloak-26.0.7/quarkus/config-api/src/main/java/org/keycloak/config/OptionBuilder.java
@@ -0,0 +1,162 @@
+package org.keycloak.config;
+
+import org.keycloak.common.util.CollectionUtil;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class OptionBuilder<T> {
+
+    private static final List<String> BOOLEAN_TYPE_VALUES = List.of(Boolean.TRUE.toString(), Boolean.FALSE.toString());
+
+    private final Class<T> type;
+    private final Class<?> auxiliaryType;
+    private final String key;
+    private OptionCategory category;
+    private boolean hidden;
+    private boolean build;
+    private String description;
+    private Optional<T> defaultValue;
+    private List<String> expectedValues = List.of();
+    // Denotes whether a custom value can be provided among the expected values
+    private boolean strictExpectedValues;
+    private DeprecatedMetadata deprecatedMetadata;
+
+    public static <A> OptionBuilder<List<A>> listOptionBuilder(String key, Class<A> type) {
+        return new OptionBuilder(key, List.class, type);
+    }
+
+    public OptionBuilder(String key, Class<T> type) {
+        this(key, type, null);
+    }
+
+    private OptionBuilder(String key, Class<T> type, Class<?> auxiliaryType) {
+        this.type = type;
+        this.auxiliaryType = auxiliaryType;
+        if (type.isArray() || ((Collection.class.isAssignableFrom(type) || Map.class.isAssignableFrom(type)) && type != java.util.List.class)) {
+            throw new IllegalArgumentException("Non-List multi-valued options are not yet supported");
+        }
+        this.key = key;
+        category = OptionCategory.GENERAL;
+        hidden = false;
+        build = false;
+        description = null;
+        defaultValue = Optional.empty();
+        strictExpectedValues = true;
+    }
+
+    public OptionBuilder<T> category(OptionCategory category) {
+        this.category = category;
+        return this;
+    }
+
+    public OptionBuilder<T> hidden() {
+        this.hidden = true;
+        return this;
+    }
+
+    public OptionBuilder<T> buildTime(boolean build) {
+        this.build = build;
+        return this;
+    }
+
+    public OptionBuilder<T> description(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public OptionBuilder<T> defaultValue(Optional<T> defaultV) {
+        this.defaultValue = defaultV;
+        return this;
+    }
+
+    public OptionBuilder<T> defaultValue(T defaultV) {
+        this.defaultValue = Optional.ofNullable(defaultV);
+        return this;
+    }
+
+    public OptionBuilder<T> expectedValues(List<String> expected) {
+        return expectedValues(true, expected);
+    }
+
+    /**
+     * @param strict   if only expected values are allowed, or some other custom value can be specified
+     * @param expected expected values
+     */
+    public OptionBuilder<T> expectedValues(boolean strict, List<String> expected) {
+        this.strictExpectedValues = strict;
+        this.expectedValues = expected;
+        return this;
+    }
+
+    public OptionBuilder<T> expectedValues(Class<? extends Enum> expected) {
+        return expectedValues(true, expected);
+    }
+
+    public OptionBuilder<T> expectedValues(boolean strict, Class<? extends Enum> expected) {
+        this.strictExpectedValues = strict;
+        this.expectedValues = Stream.of(expected.getEnumConstants()).map(Object::toString).collect(Collectors.toList());
+        return this;
+    }
+
+    public OptionBuilder<T> expectedValues(T ... expected) {
+        return expectedValues(true, expected);
+    }
+
+    /**
+     * @param strict   if only expected values are allowed, or some other custom value can be specified
+     * @param expected expected values - if empty and the {@link #type} or {@link #auxiliaryType} is enum, values are inferred
+     */
+    public OptionBuilder<T> expectedValues(boolean strict, T... expected) {
+        this.strictExpectedValues = strict;
+        this.expectedValues = Stream.of(expected).map(Object::toString).collect(Collectors.toList());
+        return this;
+    }
+
+    public OptionBuilder<T> deprecated() {
+        this.deprecatedMetadata = DeprecatedMetadata.deprecateOption(null);
+        return this;
+    }
+
+    public OptionBuilder<T> deprecatedMetadata(DeprecatedMetadata deprecatedMetadata) {
+        this.deprecatedMetadata = deprecatedMetadata;
+        return this;
+    }
+
+    public OptionBuilder<T> deprecatedValues(String note, T... values) {
+        this.deprecatedMetadata = DeprecatedMetadata.deprecateValues(note, Stream.of(values).map(Object::toString).toArray(String[]::new));
+        return this;
+    }
+
+    public Option<T> build() {
+        if (deprecatedMetadata == null && category.getSupportLevel() == ConfigSupportLevel.DEPRECATED) {
+            deprecated();
+        }
+
+        Class<?> expected = type;
+        if (auxiliaryType != null) {
+            expected = auxiliaryType;
+        }
+
+        if (CollectionUtil.isEmpty(expectedValues)) {
+            if (Boolean.class.equals(expected)) {
+                expectedValues(strictExpectedValues, BOOLEAN_TYPE_VALUES);
+            }
+
+            if (Enum.class.isAssignableFrom(expected)) {
+                expectedValues(strictExpectedValues, (Class<? extends Enum>) expected);
+            }
+        }
+
+        if (defaultValue.isEmpty() && Boolean.class.equals(expected)) {
+            defaultValue = Optional.of((T) Boolean.FALSE);
+        }
+
+        return new Option<T>(type, key, category, hidden, build, description, defaultValue, expectedValues, strictExpectedValues, deprecatedMetadata);
+    }
+
+}
---
+++ keycloak-26.0.7/quarkus/tests/integration/src/test/java/org/keycloak/it/cli/dist/QuarkusPropertiesAutoBuildDistTest.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.keycloak.it.cli.dist;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.function.Consumer;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.keycloak.it.junit5.extension.BeforeStartDistribution;
+import org.keycloak.it.junit5.extension.CLIResult;
+import org.keycloak.it.junit5.extension.DistributionTest;
+import org.keycloak.it.junit5.extension.RawDistOnly;
+import org.keycloak.it.utils.KeycloakDistribution;
+
+import io.quarkus.test.junit.main.Launch;
+import io.quarkus.test.junit.main.LaunchResult;
+
+@DistributionTest(defaultOptions = {"--http-enabled=true", "--hostname-strict=false"})
+@RawDistOnly(reason = "Containers are immutable")
+@TestMethodOrder(OrderAnnotation.class)
+public class QuarkusPropertiesAutoBuildDistTest {
+
+    @Test
+    @Launch({ "start" })
+    @Order(1)
+    void reAugOnFirstRun(LaunchResult result) {
+        CLIResult cliResult = (CLIResult) result;
+        cliResult.assertBuild();
+    }
+
+    @Test
+    @BeforeStartDistribution(EnableAdditionalConsoleHandler.class)
+    @Launch({ "start" })
+    @Order(2)
+    void testQuarkusRuntimePropDoesNotTriggerReAug(LaunchResult result) {
+        CLIResult cliResult = (CLIResult) result;
+        cliResult.assertNoBuild();
+
+        assertThat(cliResult.getOutput(), containsString("Keycloak is the best"));
+    }
+
+    @Test
+    @BeforeStartDistribution(DisableAdditionalConsoleHandler.class)
+    @Launch({ "start" })
+    @Order(3)
+    void testNoReAugAfterChangingRuntimeProperty(LaunchResult result) {
+        CLIResult cliResult = (CLIResult) result;
+        cliResult.assertNoBuild();
+        assertThat(cliResult.getOutput(), not(containsString("Keycloak is the best")));
+    }
+
+    @Test
+    @BeforeStartDistribution(AddAdditionalDatasource.class)
+    @Launch({ "start" })
+    @Order(4)
+    void testReAugForAdditionalDatasource(LaunchResult result) {
+        CLIResult cliResult = (CLIResult) result;
+        cliResult.assertBuild();
+    }
+
+    @Test
+    @BeforeStartDistribution(ChangeAdditionalDatasourceUsername.class)
+    @Launch({ "start" })
+    @Order(5)
+    void testNoReAugForAdditionalDatasourceRuntimeProperty(LaunchResult result) {
+        CLIResult cliResult = (CLIResult) result;
+        cliResult.assertNoBuild();
+    }
+
+    @Test
+    @BeforeStartDistribution(ChangeAdditionalDatasourceDbKind.class)
+    @Launch({ "start" })
+    @Order(6)
+    void testNoReAugWhenBuildTimePropertiesAreTheSame(LaunchResult result) {
+        CLIResult cliResult = (CLIResult) result;
+        cliResult.assertNoBuild();
+    }
+
+    @Test
+    @BeforeStartDistribution(AddAdditionalDatasource2.class)
+    @Launch({ "start" })
+    @Order(7)
+    void testReAugWhenAnotherDatasourceAdded(LaunchResult result) {
+        CLIResult cliResult = (CLIResult) result;
+        cliResult.assertBuild();
+    }
+
+    @Test
+    @BeforeStartDistribution(SetDatabaseKind.class)
+    @Launch({ "start" })
+    @Order(8)
+    void testWrappedBuildPropertyTriggersBuildButGetsIgnoredWhenSetByQuarkus(LaunchResult result) {
+        CLIResult cliResult = (CLIResult) result;
+        cliResult.assertBuild();
+        cliResult.assertStarted();
+    }
+
+    @Test
+    @BeforeStartDistribution(AddNonXADatasource.class)
+    @Launch({ "start" })
+    @Order(9)
+    void nonXADatasourceFailsToStart(LaunchResult result) {
+        CLIResult cliResult = (CLIResult) result;
+        cliResult.assertError("Multiple datasources are configured but more than 1 is using non-XA transactions.");
+    }
+
+    public static class EnableAdditionalConsoleHandler implements Consumer<KeycloakDistribution> {
+        @Override
+        public void accept(KeycloakDistribution distribution) {
+            distribution.setQuarkusProperty("quarkus.log.handler.console.\"console-2\".enable", "true");
+            distribution.setQuarkusProperty("quarkus.log.handler.console.\"console-2\".format", "Keycloak is the best");
+            distribution.setQuarkusProperty("quarkus.log.handlers", "console-2");
+        }
+    }
+
+    public static class DisableAdditionalConsoleHandler implements Consumer<KeycloakDistribution> {
+
+        @Override
+        public void accept(KeycloakDistribution distribution) {
+            distribution.setQuarkusProperty("quarkus.log.handler.console.\"console-2\".enable", "false");
+        }
+    }
+
+    public static class AddAdditionalDatasource implements Consumer<KeycloakDistribution> {
+        @Override
+        public void accept(KeycloakDistribution distribution) {
+            distribution.setQuarkusProperty("quarkus.datasource.user-store.db-kind", "h2");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store.username","sa");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store.jdbc.url","jdbc:h2:mem:user-store;DB_CLOSE_DELAY=-1");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store.jdbc.transactions", "xa");
+        }
+    }
+
+    public static class AddAdditionalDatasource2 implements Consumer<KeycloakDistribution> {
+        @Override
+        public void accept(KeycloakDistribution distribution) {
+            distribution.setQuarkusProperty("quarkus.datasource.user-store2.db-kind", "h2");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store2.db-transactions", "enabled");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store2.username","sa");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store2.jdbc.url","jdbc:h2:mem:user-store2;DB_CLOSE_DELAY=-1");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store2.jdbc.transactions", "xa");
+        }
+    }
+
+    public static class AddNonXADatasource implements Consumer<KeycloakDistribution> {
+        @Override
+        public void accept(KeycloakDistribution distribution) {
+            distribution.setQuarkusProperty("quarkus.datasource.user-store3.db-kind", "h2");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store3.db-transactions", "enabled");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store3.username","sa");
+            distribution.setQuarkusProperty("quarkus.datasource.user-store3.jdbc.url","jdbc:h2:mem:user-store2;DB_CLOSE_DELAY=-1");
+        }
+    }
+
+    public static class ChangeAdditionalDatasourceUsername implements Consumer<KeycloakDistribution> {
+        @Override
+        public void accept(KeycloakDistribution distribution) {
+            distribution.setQuarkusProperty("quarkus.datasource.user-store.username","foo");
+        }
+    }
+
+    public static class ChangeAdditionalDatasourceDbKind implements Consumer<KeycloakDistribution> {
+        @Override
+        public void accept(KeycloakDistribution distribution) {
+            distribution.setQuarkusProperty("quarkus.datasource.user-store.db-kind","h2");
+        }
+    }
+
+    public static class SetDatabaseKind implements Consumer<KeycloakDistribution> {
+        @Override
+        public void accept(KeycloakDistribution distribution) {
+            distribution.setManualStop(true);
+            distribution.setQuarkusProperty("quarkus.datasource.db-kind", "postgres");
+        }
+    }
+}
