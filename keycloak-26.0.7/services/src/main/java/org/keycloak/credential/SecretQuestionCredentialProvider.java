5ef4e9e0e2678691bd527539062b11a74efc79bd
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-09-11 14:10:58 +0900

    追加

---
+++ keycloak-26.0.7/services/src/main/java/org/keycloak/credential/SecretQuestionCredentialProvider.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.keycloak.credential;
+
+import org.jboss.logging.Logger;
+import org.keycloak.common.util.ObjectUtil;
+import org.keycloak.common.util.Time;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.SingleUseObjectProvider;
+import org.keycloak.models.UserCredentialModel;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.credential.SecretQuestionCredentialModel;
+import org.keycloak.models.credential.dto.SecretQuestionCredentialData;
+import org.keycloak.models.credential.dto.SecretQuestionSecretData;
+
+/**
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+public class SecretQuestionCredentialProvider implements CredentialProvider<SecretQuestionCredentialModel>, CredentialInputValidator/*, OnUserCache*/ {
+    private static final Logger logger = Logger.getLogger(SecretQuestionCredentialProvider.class);
+
+    protected KeycloakSession session;
+
+    public SecretQuestionCredentialProvider(KeycloakSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public CredentialModel createCredential(RealmModel realm, UserModel user, SecretQuestionCredentialModel credentialModel) {
+        if (credentialModel.getCreatedDate() == null) {
+            credentialModel.setCreatedDate(Time.currentTimeMillis());
+        }
+        return user.credentialManager().createStoredCredential(credentialModel);
+    }
+
+    @Override
+    public boolean deleteCredential(RealmModel realm, UserModel user, String credentialId) {
+        return user.credentialManager().removeStoredCredentialById(credentialId);
+    }
+
+    @Override
+    public SecretQuestionCredentialModel getCredentialFromModel(CredentialModel model) {
+        return SecretQuestionCredentialModel.createFromCredentialModel(model);
+    }
+
+    @Override
+    public boolean supportsCredentialType(String credentialType) {
+        return getType().equals(credentialType);
+    }
+
+    @Override
+    public boolean isConfiguredFor(RealmModel realm, UserModel user, String credentialType) {
+        if (!supportsCredentialType(credentialType)) return false;
+        return user.credentialManager().getStoredCredentialsByTypeStream(credentialType).findAny().isPresent();
+    }
+
+    public boolean isConfiguredFor(RealmModel realm, UserModel user){
+        return isConfiguredFor(realm, user, getType());
+    }
+
+    @Override
+    public boolean isValid(RealmModel realm, UserModel user, CredentialInput credentialInput) {
+        if (!(credentialInput instanceof UserCredentialModel)) {
+            logger.debug("Expected instance of UserCredentialModel for CredentialInput");
+            return false;
+
+        }
+        String challengeResponse = credentialInput.getChallengeResponse();
+        if (challengeResponse == null) {
+            return false;
+        }
+        if (ObjectUtil.isBlank(credentialInput.getCredentialId())) {
+            logger.debugf("CredentialId is null when validating credential of user %s", user.getUsername());
+            return false;
+        }
+
+        CredentialModel credential = user.credentialManager().getStoredCredentialById(credentialInput.getCredentialId());
+        SecretQuestionCredentialModel secretQuestionCredentialModel = SecretQuestionCredentialModel.createFromCredentialModel(credential);
+        SecretQuestionSecretData secretData = secretQuestionCredentialModel.getSecretQuestionSecretData();
+        SecretQuestionCredentialData credentialData = otpCredentialModel.getSecretQuestionCredentialData();
+        return true;
+    }
+
+    @Override
+    public String getType() {
+        return SecretQuestionCredentialModel.TYPE;
+    }
+
+    @Override
+    public CredentialTypeMetadata getCredentialTypeMetadata(CredentialTypeMetadataContext metadataContext) {
+        return CredentialTypeMetadata.builder()
+                .type(getType())
+                .category(CredentialTypeMetadata.Category.TWO_FACTOR)
+                .displayName("aecret-question-display-name")
+                .helpText("aecret-question-help-text")
+                .iconCssClass("kcAuthenticatorSecretQuestionClass")
+                .createAction(UserModel.RequiredAction.CONFIGURE_TOTP.toString())
+                .removeable(true)
+                .build(session);
+    }
+}
