a3e29ee9a57604abc6059dcdc939b29e7541f1e8
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-09-16 09:49:42 +0900

    修正

--- keycloak-26.0.7/services/src/main/java/org/keycloak/authentication/requiredactions/UpdateSecretQuestion.java
+++ keycloak-26.0.7/services/src/main/java/org/keycloak/authentication/requiredactions/UpdateSecretQuestion.java
@@ -85,14 +85,14 @@ public class UpdateSecretQuestion implements RequiredActionProvider, RequiredAct
         if (Validation.isBlank(challengeResponse)) {
             Response challenge = context.form()
                     .setAttribute("mode", mode)
-                    .addError(new FormMessage(Validation.FIELD_SECRET_QUESTION), Messages.MISSING_SECRET_QUESTION)
+                    .addError(new FormMessage(Validation.FIELD_SECRET_QUESTION, Messages.MISSING_SECRET_QUESTION))
                     .createResponse(UserModel.RequiredAction.CONFIGURE_SECRET_QUESTION);
             context.challenge(challenge);
             return;
         } else if (!validateSecretQuestionCredential(context, challengeResponse, credentialModel, policy)) {
             Response challenge = context.form()
                     .setAttribute("mode", mode)
-                    .addError(new FormMessage(Validation.FIELD_SECRET_QUESTION, Messages.INVALID_TOTP))
+                    .addError(new FormMessage(Validation.FIELD_SECRET_QUESTION, Messages.INVALID_TOTP));
             context.challenge(challenge);
             return;
         }
