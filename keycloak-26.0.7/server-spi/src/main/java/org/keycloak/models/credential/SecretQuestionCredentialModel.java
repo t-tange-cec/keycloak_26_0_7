1b0d48504b5f61d7e55575590688ee0b19c3a573
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-09-16 15:42:46 +0900

    修正

--- keycloak-26.0.7/server-spi/src/main/java/org/keycloak/models/credential/SecretQuestionCredentialModel.java
+++ keycloak-26.0.7/server-spi/src/main/java/org/keycloak/models/credential/SecretQuestionCredentialModel.java
@@ -21,7 +21,7 @@ public class SecretQuestionCredentialModel extends CredentialModel {
     public static SecretQuestionCredentialModel createFromPolicy(RealmModel realm, String secretValue, String userLabel) {
 
     	SecretQuestionSecretData secretData = new  SecretQuestionSecretData(secretValue,null);
-    	SecretQuestionCredentialModel credentialModel = new SecretQuestionCredentialModel(secretData);
+    	SecretQuestionCredentialModel credentialModel = new SecretQuestionCredentialModel(null,secretData);
         //credentialModel.fillCredentialModelFields();
         credentialModel.setUserLabel(userLabel);
         return credentialModel;
