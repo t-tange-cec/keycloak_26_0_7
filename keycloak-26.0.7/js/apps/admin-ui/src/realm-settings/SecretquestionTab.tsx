c38921c3f3cb3988c7117d7b36878556b7d973c7
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-09-17 09:52:42 +0900

    追加

---
+++ keycloak-26.0.7/js/apps/admin-ui/src/realm-settings/SecretquestionTab.tsx
@@ -0,0 +1,6 @@
+import { useTranslation } from "react-i18next";
+import { useConfirmDialog } from "../components/confirm-dialog/ConfirmDialog";
+
+export const SecretquestionTab = () => {
+return <div>This is SecretQuestionTab</div>;
+};
