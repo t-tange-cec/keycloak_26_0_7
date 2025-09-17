123bcdbbc7ac1c343f0d7038941bca70fe3f216a
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-09-17 19:18:47 +0900

    修正

--- keycloak-26.0.7/js/apps/admin-ui/src/realm-settings/RealmSettingsTabs.tsx
+++ keycloak-26.0.7/js/apps/admin-ui/src/realm-settings/RealmSettingsTabs.tsx
@@ -283,7 +283,7 @@ export const RealmSettingsTabs = () => {
   const themesTab = useTab("themes");
   const keysTab = useTab("keys");
   const riskbaseTab = useTab("riskbase");
-  const secretquestionTab = useTab("secret");
+  const secretquestionTab = useTab("secretquestion");
   const eventsTab = useTab("events");
   const localizationTab = useTab("localization");
   const securityDefensesTab = useTab("security-defenses");
@@ -378,8 +378,8 @@ export const RealmSettingsTabs = () => {
 		    <RiskbaseTab />
 		  </Tab>
 		  <Tab
-		    title={<TabTitleText>{t("secret")}</TabTitleText>}
-		    data-testid="rs-secret-tab"
+		    title={<TabTitleText>{t("secretquestion")}</TabTitleText>}
+		    data-testid="rs-secretquestion-tab"
 		    {...secretquestionTab}
 		  >
 		    <SecretquestionTab />
