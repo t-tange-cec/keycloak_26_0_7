80052d90ac542f08c208cfc800b1e78046c1f26d
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-09-18 09:24:10 +0900

    ブランチ指定に変更

--- keycloak_build.sh
+++ keycloak_build.sh
@@ -2,17 +2,19 @@
 
 export REPO_NAME=keycloak_26_0_7
 export REPO_USER=t-tange-cec
+export BRANCH_OPT="-b RiskBase_Add"
+#export BRANCH_OPT="-b main"
 export SOURCE_URL=https://github.com/${REPO_USER}/${REPO_NAME}.git
 export CLONE_DIR=${HOME}/${REPO_NAME}/keycloak-26.0.7
 export MAVEN_OPTS="-Xmx4g -Xms512m"
-export NODE_OPTIONS="--max-old-space-size=4096"
+export NODE_OPTIONS="--max-old-space-size=5096"
 export BUILD_DIR=${HOME}/keycloak
 export DIST_DIR=${HOME}/keycloak/distribution
 
 
 rm -rf ${HOME}/${REPO_NAME}
 rm -rf ${BUILD_DIR}
-git clone ${SOURCE_URL}
+git clone ${BRANCH_OPT}  ${SOURCE_URL}
 mv  ${CLONE_DIR} ${BUILD_DIR}
 rm -rf ${HOME}/${REPO_NAME}
 
