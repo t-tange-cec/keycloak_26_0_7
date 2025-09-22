91bae3059ab8ffa1f055411d5b1f25900cbd4bf3
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-09-22 15:19:35 +0900

    削除

--- keycloak-26.0.7/model/jpa/src/main/java/org/keycloak/models/jpa/entities/RealmEntity.java
+++ keycloak-26.0.7/model/jpa/src/main/java/org/keycloak/models/jpa/entities/RealmEntity.java
@@ -221,9 +221,6 @@ public class RealmEntity {
     @Column(name="INTERNATIONALIZATION_ENABLED")
     protected boolean internationalizationEnabled;
 
-    @Column(name="RISKBASE_ENABLED")
-    protected boolean riskbaseEnabled;
-
     @ElementCollection
     @Column(name="VALUE")
     @CollectionTable(name="REALM_SUPPORTED_LOCALES", joinColumns={ @JoinColumn(name="REALM_ID") })
@@ -619,14 +616,6 @@ public class RealmEntity {
 
     public void setInternationalizationEnabled(boolean internationalizationEnabled) {
         this.internationalizationEnabled = internationalizationEnabled;
-    }
-
-    public boolean isRiskbaseEnabled() {
-        return riskbaseEnabled;
-    }
-
-    public void setRiskbaseEnabled(boolean riskbaseEnabled) {
-        this.riskbaseEnabled = riskbaseEnabled;
     }
     
     public Set<String> getSupportedLocales() {
