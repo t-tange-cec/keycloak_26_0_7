da319f3cfc8ee35695656cd6a6c032868c1f9e8f
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-01 19:36:27 +0900

    調査用

--- keycloak-26.0.7/services/src/main/java/org/keycloak/authentication/authenticators/browser/PasswordFormFactory.java
+++ keycloak-26.0.7/services/src/main/java/org/keycloak/authentication/authenticators/browser/PasswordFormFactory.java
@@ -44,7 +44,7 @@ public class PasswordFormFactory implements AuthenticatorFactory {
 
     @Override
     public void init(Config.Scope config) {
-
+    	System.out.println("PasswordFormFactory initialized");
     }
 
     @Override
