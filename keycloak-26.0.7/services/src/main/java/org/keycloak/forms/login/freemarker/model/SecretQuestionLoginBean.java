908d6460bb16115d8817c816c4d3b0a70797dc62
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-03 11:08:54 +0900

    削除

--- keycloak-26.0.7/services/src/main/java/org/keycloak/forms/login/freemarker/model/SecretQuestionLoginBean.java
+++ keycloak-26.0.7/services/src/main/java/org/keycloak/forms/login/freemarker/model/SecretQuestionLoginBean.java
@@ -21,7 +21,6 @@ package org.keycloak.forms.login.freemarker.model;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.keycloak.authentication.authenticators.browser.RiskBaseFormAuthenticator;
 import org.keycloak.credential.CredentialModel;
 import org.keycloak.credential.CredentialProvider;
 import org.keycloak.credential.SecretQuestionCredentialProvider;
@@ -77,7 +76,7 @@ public class SecretQuestionLoginBean {
         public SecretQuestionCredential(CredentialModel credentialModel) {
             this.id = credentialModel.getId();
             // TODO: "Unnamed" OTP credentials should be displayed in the UI in gray
-            this.userLabel = credentialModel.getUserLabel() == null || credentialModel.getUserLabel().isEmpty() ? RiskBaseFormAuthenticator.UNNAMED : credentialModel.getUserLabel();
+            this.userLabel = credentialModel.getUserLabel() == null || credentialModel.getUserLabel().isEmpty() ? "unnamed" : credentialModel.getUserLabel();
         }
 
         public String getId() {
