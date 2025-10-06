32944a83a7852f4660108554ff4a3253c3697bd8
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-06 20:07:37 +0900

    修正

--- riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/authenticator/ConditionalRiskbase.java
+++ riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/authenticator/ConditionalRiskbase.java
@@ -39,8 +39,10 @@ public class ConditionalRiskbase implements Authenticator {
 				Integer score = factory.getScore(context);
 				logger.info("score:" + Integer.toString(score));
 				if (score < threashold) {
+					logger.info("success:");					
 					context.success();
 				} else {
+					logger.info("attempted:");			
 					// スキップまたは失敗
 					context.attempted(); // スキップ扱い
 				}
