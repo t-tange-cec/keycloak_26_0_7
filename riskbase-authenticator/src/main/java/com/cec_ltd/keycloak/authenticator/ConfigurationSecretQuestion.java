b0655915c97055dafc8dbe90599c28d302606cb0
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-08 20:42:41 +0900

    修正

--- riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/authenticator/ConfigurationSecretQuestion.java
+++ riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/authenticator/ConfigurationSecretQuestion.java
@@ -23,30 +23,23 @@ import org.jboss.logging.Logger;
 
 public class ConfigurationSecretQuestion implements Authenticator {
 	private static final Logger logger = Logger.getLogger(ConfigurationSecretQuestion.class);
-	private static String FORM_NAME="login-update-secret-question.ftl";
+	private static String FORM_NAME = "login-update-secret-question.ftl";
+
 	@Override
 	public void authenticate(AuthenticationFlowContext context) {
 		UserModel user = context.getUser();
 		RealmModel realm = context.getRealm();
-		String enableRiskbase = realm.getAttribute("EnableRiskbase");
 		try {
-			if (StringUtil.isNullOrEmpty(enableRiskbase)) {
-				enableRiskbase = "true";
+			String qid = user.getFirstAttribute("qid");
+			if (StringUtil.isNullOrEmpty(qid)) {
+				logger.info("qid: null");
+				LoginFormsProvider provider = context.form();
+				logger.info(FORM_NAME);
+				context.forceChallenge(provider.createForm(FORM_NAME));
+				return;
 			}
-			logger.info("enableRiskbase: " + enableRiskbase);
-			if ("true".equalsIgnoreCase(enableRiskbase)) {
-				String qid = user.getFirstAttribute("qid");
-				if (StringUtil.isNullOrEmpty(qid)) {
-					logger.info("qid: null");
-					LoginFormsProvider provider = context.form();
-					logger.info(FORM_NAME);
-					context.forceChallenge(provider.createForm(FORM_NAME));
-					return;
-				} else {
-					logger.info("qid: "+qid);
-				}
-			}
-			context.success(); // スキップ扱い
+			logger.info("success");
+			context.success();
 		} catch (Exception e) {
 			context.failure(AuthenticationFlowError.INTERNAL_ERROR);
 			logger.error("Authentication failed", e);
@@ -61,8 +54,8 @@ public class ConfigurationSecretQuestion implements Authenticator {
 		MultivaluedMap<String, String> map = request.getDecodedFormParameters();
 		String answer = map.getFirst("secretAnswer");
 		String qid = map.getFirst("qid");
-		logger.info("qid: "+qid);
-		logger.info("secretAnswer: "+answer);
+		logger.info("qid: " + qid);
+		logger.info("secretAnswer: " + answer);
 		if (StringUtil.isNullOrEmpty(answer) || StringUtil.isNullOrEmpty(qid)) {
 			context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS,
 					context.form().setError("質問または回答が入力されていません").createForm(FORM_NAME));
@@ -75,6 +68,7 @@ public class ConfigurationSecretQuestion implements Authenticator {
 				.getProvider(CredentialProvider.class, "secret-question");
 		provider.createCredential(realm, user, credential);
 		user.setSingleAttribute("qid", qid);
+		logger.info("success");
 		context.success();
 	}
 
