d7b5d17f205ca804e2a14bd5d1c3c755d295ae07
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-08 19:35:44 +0900

    修正

--- riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/authenticator/SecretQuestionForm.java
+++ riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/authenticator/SecretQuestionForm.java
@@ -71,10 +71,11 @@ public class SecretQuestionForm implements Authenticator {
 				logger.info("provider:not null");
 			}
 			boolean valid = provider.isValid(realm, user, input);
-//			if (!valid) {
-//				context.failure(AuthenticationFlowError.INVALID_CREDENTIALS);
-//				return;
-//			}
+			if (!valid) {
+				logger.info("failure");
+				context.failure(AuthenticationFlowError.INVALID_CREDENTIALS);
+				return;
+			}
 			logger.info("success:");
 			context.success();
 		} catch (Exception e) {
