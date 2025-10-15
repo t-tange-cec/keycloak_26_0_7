79eda2d2919da094b65600fa1f3b76ffd3e111b4
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-15 17:51:52 +0900

    修正

--- riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/authenticator/ConditionalRiskbase.java
+++ riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/authenticator/ConditionalRiskbase.java
@@ -27,7 +27,7 @@ public class ConditionalRiskbase implements Authenticator {
 			}
 			logger.info("enableRiskbase: " + enableRiskbase);
 			if ("true".equalsIgnoreCase(enableRiskbase)) {
-				String strThreahhold = realm.getAttribute("threashhold");
+				String strThreahhold = realm.getAttribute("threshhold");
 				if (StringUtil.isNullOrEmpty(strThreahhold)) {
 					strThreahhold = "50";
 				}
