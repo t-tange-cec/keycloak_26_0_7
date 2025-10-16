02dde6d94bc84078a7dad55e4aad6a8ecf12127d
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-16 16:17:34 +0900

    修正

--- riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/risk/api/AdminUIRest.java
+++ riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/risk/api/AdminUIRest.java
@@ -1,14 +1,20 @@
 package com.cec_ltd.keycloak.risk.api;
 
+import java.util.List;
+import java.util.UUID;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
-import org.hibernate.mapping.List;
 import org.keycloak.models.jpa.entities.RealmRiskBase;
 
 public class AdminUIRest {
 	@PostMapping("/admin/riskbase")
 	public ResponseEntity<?> saveRiskItems(@RequestBody List<RiskItemDto> items) {
 	    for (RiskItemDto dto : items) {
-	        RealmEntity realm = realmRepository.findById(dto.getRealmId());
+	        Object realmRepository;
+			RealmEntity realm = realmRepository.findById(dto.getRealmId());
 	        RealmRiskBase entity = new RealmRiskBase();
 	        entity.setId(UUID.randomUUID().toString());
 	        entity.setRealm(realm);
