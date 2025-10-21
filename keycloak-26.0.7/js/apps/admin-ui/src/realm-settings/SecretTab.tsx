9dc7b51ecbcc9b3f57e257e5e392201ef86f0e74
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-21 17:53:37 +0900

    修正

--- keycloak-26.0.7/js/apps/admin-ui/src/realm-settings/SecretTab.tsx
+++ keycloak-26.0.7/js/apps/admin-ui/src/realm-settings/SecretTab.tsx
@@ -62,11 +62,11 @@ export const SecretTab = ({
 		  className="pf-v5-u-mt-lg"
 		  onSubmit={handleSubmit(onSubmit)}
 		>
-		<Button onClick={() => setQuestions([...questions, { id: `PI${questions.length + 1}`, jp: "", en: "" }])}>
+		<Button onClick={() => setQuestions([...questions, { id: `PI${String(questions.length + 1).padStart(3, '0')}`, jp: "", en: "" }])}>
 		{t("add")}
 		</Button>
 		<div>
-		   <h2>%u79D8%u5BC6%u306E%u8CEA%u554F%u4E00%u89A7</h2>
+		   <h2>”é–§‚ÌŽ¿–âˆê——</h2>
 		   <table>
 		     <thead>
 		       <tr>
