0368a03b1aa3e2cc6a76db36839c9088bc3cf80c
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-21 10:54:48 +0900

    修正

--- riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/risk/repository/RealmRiskBaseRepository.java
+++ riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/risk/repository/RealmRiskBaseRepository.java
@@ -1,7 +1,10 @@
 package com.cec_ltd.keycloak.risk.repository;
 
+import java.util.List;
+
 import org.keycloak.models.jpa.entities.RealmRiskBase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RealmRiskBaseRepository extends JpaRepository<RealmRiskBase, String> {
+	List<RealmRiskBase> findByRealmId(String realmId);
 }
