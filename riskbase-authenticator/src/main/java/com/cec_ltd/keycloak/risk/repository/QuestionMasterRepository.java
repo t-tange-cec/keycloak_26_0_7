c36ef2822b3ef38556edcd8d2ee1c244cfbd2d5c
t-tange-cec <t-tange-cec@cec-ltd.co.jp>
2025-10-21 10:33:07 +0900

    修正

--- riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/risk/repository/QuestionMasterRepository.java
+++ riskbase-authenticator/src/main/java/com/cec_ltd/keycloak/risk/repository/QuestionMasterRepository.java
@@ -2,6 +2,8 @@ package com.cec_ltd.keycloak.risk.repository;
 
 import org.keycloak.models.jpa.entities.QuestionMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 
 public interface QuestionMasterRepository extends JpaRepository<QuestionMaster, String> {
     List<QuestionMaster> findByRealmIdAndLocale(String realmId, String locale);
